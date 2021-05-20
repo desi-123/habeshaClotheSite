de38f6fb495ce306921dbd587ceefa68549e3ba4
Desalegn Wagaw <desiemeng21m@gmail.com>
2021-05-20 08:52:06 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -33,21 +33,19 @@ app.use('/api/upload', uploadRoutes)
 app.use('/api/config/paypal', (req, res) => 
     res.send(process.env.PAYPAL_CLIENT_ID))
 
-//const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+    app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-    app.get('*', (req, res) => 
+    if (process.env.NODE_ENV === 'production') {
+      app.use(express.static(path.join(__dirname, '/frontend/build')))
+    
+      app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    )
-} else {
-    app.get('/', (req, res) => {
+      )
+    } else {
+      app.get('/', (req, res) => {
         res.send('API is running....')
-    })
-}
+      })
+    }
 
 
 
